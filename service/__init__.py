667609977926ff40efd08291054407e33372e619
makresh-dev <makreshnayak@gmail.com>
2026-08-11 13:46:54 -0400

    updated

--- service/__init__.py
+++ service/__init__.py
@@ -7,8 +7,8 @@ from flask import Flask
 app = Flask(__name__)
 
 # This must be imported after the Flask app is created
-from service.common import log_handlers  # pylint: disable=wrong-import-position
-from service import routes  # pylint: disable=wrong-import-position,cyclic-import
+from service.common import log_handlers
+from service import routes
 
 log_handlers.init_logging(app, "gunicorn.error")
 
